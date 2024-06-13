10214f881ac4d7387c899d4a340a721511e20fe6
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-06-14 01:36:23 +0700

    fix: video list better alignment

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -43,12 +43,21 @@ const VideoRecommendations: React.FC = () => {
       <div>
         {Array.isArray(videos) && videos.length > 0 ? (
           videos.map((video) => (
-            <div key={video.id.videoId} className="my-4">
-              <h3>{video.snippet.title}</h3>
-              <p>{video.snippet.description}</p>
+            <div
+              key={video.id.videoId}
+              className="my-4 flex flex-row items-center mr-8">
+              <div className="flex flex-col">
+                <h3 className="break-words max-w-lg">
+                  Title: {video.snippet.title}
+                </h3>
+                <p className="break-words max-w-lg mt-4">
+                  Description: {video.snippet.description}
+                </p>
+              </div>
+
               <iframe
-                width="560"
-                height="315"
+                width="250"
+                height="160"
                 src={`https://www.youtube.com/embed/${video.id.videoId}`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
