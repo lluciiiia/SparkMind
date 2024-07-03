7766e9e806862836a244178cff5809802de8e691
Mike Odnis <airwolf635@gmail.com>
2024-07-03 15:22:28 -0400

    bug: build error resolved

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,37 +1,38 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
-  try {
-    const url = new URL(req.url);
-    const query = url.searchParams.get("query");
+    try {
+        const query = req.nextUrl.searchParams.get("query");
 
-    if (!query) {
-      return NextResponse.json(
-        { error: "Error extracting text" },
-        { status: 400 }
-      );
+        if (!query) {
+            return NextResponse.json(
+                { error: "Error extracting text" },
+                { status: 400 }
+            );
+        }
+
+        const response = await axios.get(
+            "https://www.googleapis.com/youtube/v3/search",
+            {
+                params: {
+                    part: "snippet",
+                    q: query,
+                    type: "video",
+                    maxResults: 10,
+                    key: process.env.YOUTUBE_API_KEY,
+                },
+            }
+        );
+
+        return NextResponse.json({ body: response.data.items }, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching data from YouTube:", error);
+        return NextResponse.json(
+            { error: "Failed to fetch data from YouTube" },
+            { status: 500 }
+        );
     }
-
-    const response = await axios.get(
-      "https://www.googleapis.com/youtube/v3/search",
-      {
-        params: {
-          part: "snippet",
-          q: query,
-          type: "video",
-          maxResults: 10,
-          key: process.env.YOUTUBE_API_KEY,
-        },
-      }
-    );
-
-    return NextResponse.json({ body: response.data.items }, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching data from YouTube:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch data from YouTube" },
-      { status: 500 }
-    );
-  }
 }
