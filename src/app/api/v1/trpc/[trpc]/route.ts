dc750576cba468539a3e9bc71f10154a346897bc
Mike Odnis <airwolf635@gmail.com>
2024-07-13 04:56:28 -0400

    altert: this is important before we go into production

--- src/app/api/v1/trpc/[trpc]/route.ts
+++ src/app/api/v1/trpc/[trpc]/route.ts
@@ -1,7 +1,7 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import type { NextRequest } from 'next/server';
 
-import { env } from '@/env.js'
+import { env } from '@/env'
 import { appRouter } from '@/server/api/root';
 import { createTRPCContext } from '@/server/api/trpc';
 
@@ -21,12 +21,9 @@ const handler = (req: NextRequest) =>
     req,
     router: appRouter,
     createContext: () => createContext(req),
-    onError:
-      env.NODE_ENV === 'development'
-        ? ({ path, error }) => {
-            console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
-          }
-        : undefined,
+    onError: ({ path, error }) => {
+      console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
+    },
   });
 
 export { handler as GET, handler as POST };
