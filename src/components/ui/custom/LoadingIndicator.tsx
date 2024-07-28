80ea843d57ed3bfe4cbae71eb95bfabc8f2a3184
Krishna <104525693+KRISHNA-DHARSANDIA@users.noreply.github.com>
2024-07-29 01:03:46 +0530

    Integrated Gemeni AI discussion functionality

---
+++ src/components/ui/custom/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@/styles/css/LoadingIndicator.css';
+
+
+const LoadingIndicator: React.FC = () => (
+    <div className="loader"></div> 
+);
+
+export default LoadingIndicator;
