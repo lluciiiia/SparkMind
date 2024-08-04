3360914cb68202422422ce8fdef6ce95822027aa
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-04 11:10:56 +0700

    fix: video recommendation UI

--- src/app/(dashboard)/dashboard/_components/cards/video-recommendation.tsx
+++ src/app/(dashboard)/dashboard/_components/cards/video-recommendation.tsx
@@ -5,20 +5,20 @@ import { VideoItem, VideoCardProps } from "../interfaces";
 const VideoCard: React.FC<VideoCardProps> = ({ videos }) => {
   return (
     <Card className="w-full h-[calc(100vh-56px-64px-20px-24px-56px-48px-40px)] rounded-t-3xl">
-      <div className="flex flex-col h-full overflow-y-auto rounded-t-3xl justify-center items-center">
+      <div className="flex flex-col h-full overflow-y-auto rounded-t-3xl items-center">
         {Array.isArray(videos) && videos.length > 0 ? (
           videos.map((video) => (
             <div
               key={video.id.videoId}
-              className="flex flex-row justify-between px-8 py-4 border-b border-gray-200">
-              <div className="flex flex-col mr-8">
+              className="w-full flex flex-row px-8 py-4 border-b border-gray-200">
+              <div className="flex flex-col mr-8 l-0">
                 <h3 className="break-words font-bold">{video.snippet.title}</h3>
                 <p className="break-words mt-2">
                   Description: {video.snippet.description}
                 </p>
               </div>
               <iframe
-                className="flex-shrink-0"
+                className="flex-shrink-0 ml-auto"
                 width="120"
                 height="100"
                 src={`https://www.youtube.com/embed/${video.id.videoId}`}
@@ -28,7 +28,9 @@ const VideoCard: React.FC<VideoCardProps> = ({ videos }) => {
             </div>
           ))
         ) : (
-          <p>No videos found</p>
+          <div className="flex h-full justify-center items-center">
+            <p>No videos found</p>
+          </div>
         )}
       </div>
     </Card>
