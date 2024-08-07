755bc7537114d3779da159b085c07f9a5a0683a4
Lucia (Seokyung Kim) <111489817+lluciiiia@users.noreply.github.com>
2024-08-07 09:22:29 +0700

    Revert "fix/Action Item and Google Authentication "

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -22,14 +22,14 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
+        caption: "flex justify-left pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute left-1",
+        nav_button_previous: "absolute right-10",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
