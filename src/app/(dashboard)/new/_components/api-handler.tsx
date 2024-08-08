0e6b3a67356e93c0eb167674694475493b1c0e43
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-09 02:32:47 +0700

    fix!: save-output request format

--- src/app/(dashboard)/new/_components/api-handler.tsx
+++ src/app/(dashboard)/new/_components/api-handler.tsx
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getYoutubeResponse = async (input: string) => {
   const params: { query: string; pageToken?: string | null } = {
     query: input,
   };
 
-  const response = await axios.get('/api/v1/youtube', { params });
+  const response = await axios.get("/api/v1/youtube", { params });
 
   return { data: response.data };
 };
 
 export const saveOutput = async (input: string, myLearningId: string) => {
-  const body = { input, id: myLearningId };
-
-  const response = await axios.post('/api/v1/save-output', body);
+  const response = await axios.post(`/api/v1/save-output?id=${myLearningId}`, {
+    input: input,
+  });
+  console.log("response: ", response);
 
   return { data: response.data };
 };
