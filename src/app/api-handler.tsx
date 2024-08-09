8c83b7a5e58bb7980acbfcf312110beead4f2e9d
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-09 21:50:37 +0700

    refactor: having only one api-handler in the client side

--- src/app/api-handler.tsx
+++ src/app/api-handler.tsx
@@ -14,7 +14,15 @@ export const saveOutput = async (input: string, myLearningId: string) => {
   const response = await axios.post(`/api/v1/save-output?id=${myLearningId}`, {
     input: input,
   });
-  console.log("response: ", response);
 
+  return { data: response.data };
+};
+
+export const getOutputResponse = async (myLearningId: string) => {
+  const params: { id: string } = {
+    id: myLearningId,
+  };
+
+  const response = await axios.get("/api/v1/get-output", { params });
   return { data: response.data };
 };
