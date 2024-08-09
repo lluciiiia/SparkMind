c07a5b5c9391f2eb4e35e65c8aa979f35af3310c
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-10 05:16:15 +0700

    feat!: note ai features integration

--- src/app/api-handler.tsx
+++ src/app/api-handler.tsx
@@ -52,3 +52,17 @@ export const getNotes = async (myLearningId: string) => {
   const response = await axios.get(`/api/v1/notes?id=${myLearningId}`);
   return { data: response.data };
 };
+
+export const getGrammarNote = async (content: string) => {
+  const response = await axios.post(`/api/v1/note-ai/grammar`, {
+    note: content,
+  });
+  return { data: response.data };
+};
+
+export const getConciseNote = async (content: string) => {
+  const response = await axios.post(`/api/v1/note-ai/concise`, {
+    note: content,
+  });
+  return { data: response.data };
+};
