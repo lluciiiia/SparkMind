59606c333df36cf85204578fdbf527cc318e9654
Wisesofthemall <foxxydieujuste@gmail.com>
2024-08-10 03:21:15 -0400

    feat: Complete redirect task

--- src/app/(auth)/signin/[id]/page.tsx
+++ src/app/(auth)/signin/[id]/page.tsx
@@ -1,17 +1,18 @@
-import { Card } from '@/components/ui/card';
-import { Separator } from '@/components/ui/separator';
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import {
   getAuthTypes,
   getDefaultSignInView,
   getRedirectMethod,
   getViewTypes,
-} from '@/utils/auth/settings';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
-import Image from 'next/image';
-import { redirect } from 'next/navigation';
+} from "@/utils/auth/settings";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import logo from "../../../../../public/assets/images/home/Logowithtext.png";
 
-import { BackgroundBeams } from '@/components';
+import { BackgroundBeams } from "@/components";
 
 import {
   EmailSignIn,
@@ -20,7 +21,7 @@ import {
   PasswordSignIn,
   SignUp,
   UpdatePassword,
-} from '@/components/ui/auth';
+} from "@/components/ui/auth";
 
 export default async function SignIn({
   params,
@@ -37,10 +38,11 @@ export default async function SignIn({
   let viewProp: string;
 
   // Assign url id to 'viewProp' if it's a valid string and ViewTypes includes it
-  if (typeof params.id === 'string' && viewTypes.includes(params.id)) {
+  if (typeof params.id === "string" && viewTypes.includes(params.id)) {
     viewProp = params.id;
   } else {
-    const preferredSignInView = cookies().get('preferredSignInView')?.value || null;
+    const preferredSignInView =
+      cookies().get("preferredSignInView")?.value || null;
     viewProp = getDefaultSignInView(preferredSignInView);
     return redirect(`/signin/${viewProp}`);
   }
@@ -52,10 +54,10 @@ export default async function SignIn({
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (user && viewProp !== 'update_password') {
-    return redirect('/');
-  } else if (!user && viewProp === 'update_password') {
-    return redirect('/signin');
+  if (user && viewProp !== "update_password") {
+    return redirect("/");
+  } else if (!user && viewProp === "update_password") {
+    return redirect("/signin");
   }
 
   return (
@@ -63,18 +65,18 @@ export default async function SignIn({
       <div className="absolute top-0 left-0 flex justify-center h-screen w-screen z-50">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <div className="flex justify-center pb-12 ">
-            <Image src="/logo.png" alt="logo" width={64} height={64} />
+            <Image src={logo} alt="logo" width={250} height={250} />
           </div>
           <Card
             title={
               // viewProp === 'forgot_password'
               //   ? 'Reset Password'
               //:
-              viewProp === 'update_password'
-                ? 'Update Password'
-                : viewProp === 'signup'
-                  ? 'Sign Up'
-                  : 'Sign In'
+              viewProp === "update_password"
+                ? "Update Password"
+                : viewProp === "signup"
+                ? "Sign Up"
+                : "Sign In"
             }
           >
             {/* {viewProp === 'password_signin' && (
@@ -98,13 +100,15 @@ export default async function SignIn({
             {viewProp === 'signup' && (
               <SignUp allowEmail={allowEmail} redirectMethod={redirectMethod} />
             )} */}
-            {viewProp !== 'update_password' && viewProp !== 'signup' && allowOauth && (
-              <>
-                {/* <span className="text-center">Third-party sign-in</span> */}
-                {/* <Separator /> */}
-                <OauthSignIn />
-              </>
-            )}
+            {viewProp !== "update_password" &&
+              viewProp !== "signup" &&
+              allowOauth && (
+                <>
+                  {/* <span className="text-center">Third-party sign-in</span> */}
+                  {/* <Separator /> */}
+                  <OauthSignIn />
+                </>
+              )}
           </Card>
         </div>
       </div>
