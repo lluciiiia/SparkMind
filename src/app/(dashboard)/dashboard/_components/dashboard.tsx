4ff9db7b7b8c61b62171890dd3b2560ba444b0b4
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-17 05:00:33 +0700

    chore!: delete redundant file

--- src/app/(dashboard)/dashboard/_components/dashboard.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { ContentLayout } from '@/components/dashboard/content-layout';
+import { ContentLayout } from "@/components/dashboard/content-layout";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,55 +8,59 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Card } from '@/components/ui/card';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-import { motion } from 'framer-motion';
-import { Triangle } from 'lucide-react';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
-import { useRef, useState } from 'react';
-import { FaCaretLeft, FaCaretRight, FaTimes } from 'react-icons/fa';
-import { PiNoteBlankFill } from 'react-icons/pi';
-import { useIsomorphicLayoutEffect, useMediaQuery } from 'usehooks-ts';
-import { z } from 'zod';
-import { NewNoteSection } from '../../notes/_components';
+} from "@/components/ui/breadcrumb";
+import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { motion } from "framer-motion";
+import { Triangle } from "lucide-react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import { useRef, useState } from "react";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+import { PiNoteBlankFill } from "react-icons/pi";
+import { useIsomorphicLayoutEffect, useMediaQuery } from "usehooks-ts";
+import { NewNoteSection } from "../../notes/_components";
 import {
   type FurtherInfo,
-  Message,
   type Note,
   type Output,
   type ParsedVideoData,
-  Props,
   type Question,
-  Transcript,
   type VideoItem,
-} from './interfaces';
+} from "./interfaces";
 
-import ActionCard from './cards/ActionCard';
-import SummaryCard from './cards/SummaryCard';
-import VideoCard from './cards/VideoCard';
-//discuss with AI Imports
-import DiscussionWithAI from './discussion-with-ai';
-import NoteCard from './note';
+import ActionCard from "./cards/ActionCard";
+import SummaryCard from "./cards/SummaryCard";
+import VideoCard from "./cards/VideoCard";
+import DiscussionWithAI from "./discussion-with-ai";
 
-import { API_KEY } from '@/app/api/v1/gemini-settings';
+import { API_KEY } from "@/app/api/v1/gemini-settings";
 
-import axios from 'axios';
-import { createNote, deleteNote, editNote, getNotes, getOutput } from '../../../api-handler';
-import FurtherInfoCard from './cards/FurtherInfo';
-import QuestionAndAnswer from './cards/QuestionAndAnswer';
+import {
+  createNote,
+  deleteNote,
+  editNote,
+  getNotes,
+  getOutput,
+} from "../../../api-handler";
+import FurtherInfoCard from "./cards/FurtherInfo";
+import QuestionAndAnswer from "./cards/QuestionAndAnswer";
 
 export const Dashboard = () => {
   if (!API_KEY) {
-    console.error('Missing API key');
+    console.error("Missing API key");
   }
 
   const [isOpen, setIsOpen] = useState(false);
   const [notes, setNotes] = useState<Note[]>([]);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState('summary');
+  const [activeTab, setActiveTab] = useState("summary");
   const drawerRef = useRef<HTMLDivElement>(null);
   const [showText, setShowText] = useState(false);
 
@@ -67,7 +71,7 @@ export const Dashboard = () => {
   const [furtherInfoData, setFurtherInfoData] = useState<any[]>([]);
 
   const [output, setOutput] = useState<Output | null>(null);
-  const myLearningId = searchParams.get('id');
+  const myLearningId = searchParams.get("id");
 
   useEffect(() => {
     const fetchData = async (myLearningId: string) => {
@@ -79,14 +83,14 @@ export const Dashboard = () => {
 
         setNotes(noteResponse.data.body);
       } catch (error) {
-        throw new Error('Error fetching data : ' + (error as Error).message);
+        throw new Error("Error fetching data : " + (error as Error).message);
       }
     };
 
     if (myLearningId) fetchData(myLearningId);
 
     return () => {
-      console.log('Output retrieved');
+      console.log("Output retrieved");
     };
   }, []);
 
@@ -132,13 +136,13 @@ export const Dashboard = () => {
         setIsDrawerOpen(false);
       }
     };
-    window.addEventListener('click', handleClickOutside);
+    window.addEventListener("click", handleClickOutside);
     return () => {
-      window.removeEventListener('click', handleClickOutside);
+      window.removeEventListener("click", handleClickOutside);
     };
   }, [drawerRef, isDrawerOpen, isOpen]);
 
-  const isLaptop = useMediaQuery('(min-width: 1023px)');
+  const isLaptop = useMediaQuery("(min-width: 1023px)");
 
   const handleDelete = async (id: string) => {
     const response = await deleteNote(id);
@@ -167,22 +171,26 @@ export const Dashboard = () => {
 
     const updatedNote = {
       ...selectedNote,
-      title: selectedNote.title ? selectedNote.title : 'Undefined',
+      title: selectedNote.title ? selectedNote.title : "Undefined",
       content: selectedNote.content,
     };
 
-    const response = await editNote(updatedNote.id, updatedNote.title, updatedNote.content);
+    const response = await editNote(
+      updatedNote.id,
+      updatedNote.title,
+      updatedNote.content
+    );
 
     setNotes(notes.map((note) => (note.id === id ? updatedNote : note)));
     setIsDrawerOpen(false);
   };
 
   const tabs = [
-    { name: 'summary', label: 'Summary' },
-    { name: 'video', label: 'Video recommendation' },
-    { name: 'qna', label: 'Q&A' },
-    { name: 'further-info', label: 'Further Information' },
-    { name: 'action-items', label: 'Action Items' },
+    { name: "summary", label: "Summary" },
+    { name: "video", label: "Video recommendation" },
+    { name: "qna", label: "Q&A" },
+    { name: "further-info", label: "Further Information" },
+    { name: "action-items", label: "Action Items" },
   ];
 
   return (
@@ -193,16 +201,14 @@ export const Dashboard = () => {
           onToggle={() => setIsOpen(!isOpen)}
           className="w-full"
           initial={{ width: 30 }}
-          animate={{ width: isOpen ? '100%' : 50 }}
-          transition={{ type: 'spring', stiffness: 100 }}
-        >
+          animate={{ width: isOpen ? "100%" : 50 }}
+          transition={{ type: "spring", stiffness: 100 }}>
           <summary
             className={`left-0 relative p-2 ${
-              isOpen ? 'rounded-l-md' : 'rounded-md'
+              isOpen ? "rounded-l-md" : "rounded-md"
             } bg-navy text-white rounded-r-none w-full flex items-center justify-start ${
-              isOpen ? 'justify-start' : 'justify-center'
-            }`}
-          >
+              isOpen ? "justify-start" : "justify-center"
+            }`}>
             {isOpen ? <FaCaretLeft size={24} /> : <FaCaretRight size={24} />}
             <PiNoteBlankFill size={24} />
 
@@ -237,36 +243,45 @@ export const Dashboard = () => {
                 <button
                   type="button"
                   className={`px-6 py-2 cursor-pointer ${
-                    activeTab === tab.name ? 'bg-navy text-white rounded-t-3xl' : 'text-gray'
+                    activeTab === tab.name
+                      ? "bg-navy text-white rounded-t-3xl"
+                      : "text-gray"
                   }`}
-                  onClick={() => setActiveTab(tab.name)}
-                >
+                  onClick={() => setActiveTab(tab.name)}>
                   {tab.label}
                 </button>
               </li>
             ))}
           </menu>
           {[
-            { tab: 'summary' },
-            { tab: 'video' },
-            { tab: 'qna' },
-            { tab: 'further-info' },
-            { tab: 'action-items' },
+            { tab: "summary" },
+            { tab: "video" },
+            { tab: "qna" },
+            { tab: "further-info" },
+            { tab: "action-items" },
           ].map(
             ({ tab }) =>
               activeTab === tab && (
                 <div className="rounded-b-3xl bg-white h-full" key={tab}>
-                  {activeTab === tab && tab === 'summary' && summaryData != null && (
-                    <SummaryCard summaryData={summaryData} />
+                  {activeTab === tab &&
+                    tab === "summary" &&
+                    summaryData != null && (
+                      <SummaryCard summaryData={summaryData} />
+                    )}
+                  {activeTab === tab && tab === "video" && (
+                    <VideoCard videos={videos} />
                   )}
-                  {activeTab === tab && tab === 'video' && <VideoCard videos={videos} />}
-                  {activeTab === tab && tab === 'qna' && questions.length > 0 && (
-                    <QuestionAndAnswer questions={questions} />
-                  )}
-                  {activeTab === tab && tab === 'further-info' && furtherInfoData != null && (
-                    <FurtherInfoCard furtherInfo={furtherInfoData} />
-                  )}
-                  {activeTab === tab && tab === 'action-items' && (
+                  {activeTab === tab &&
+                    tab === "qna" &&
+                    questions.length > 0 && (
+                      <QuestionAndAnswer questions={questions} />
+                    )}
+                  {activeTab === tab &&
+                    tab === "further-info" &&
+                    furtherInfoData != null && (
+                      <FurtherInfoCard furtherInfo={furtherInfoData} />
+                    )}
+                  {activeTab === tab && tab === "action-items" && (
                     <ActionCard learningId={myLearningId} />
                   )}
                   {activeTab != tab && (
@@ -275,39 +290,41 @@ export const Dashboard = () => {
                     />
                   )}
                 </div>
-              ),
+              )
           )}
         </section>
         <footer className=" w-fit flex-col bottom-0 left-0 right-0 mx-auto flex items-center justify-center">
           <motion.div
-            initial={{ y: '90%' }}
-            animate={{ y: isDrawerOpen ? 100 : '100%' }}
-            transition={{ type: 'spring', stiffness: 50 }}
+            initial={{ y: "90%" }}
+            animate={{ y: isDrawerOpen ? 100 : "100%" }}
+            transition={{ type: "spring", stiffness: 50 }}
             className={`
                 absolute flex flex-col items-center justify-center bottom-6
               `}
-            ref={drawerRef}
-          >
+            ref={drawerRef}>
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger>
                   <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
-                    transition={{ type: 'spring', stiffness: 100 }}
+                    transition={{ type: "spring", stiffness: 100 }}
                     className={`w-5 h-5 bottom-0 cursor-pointer mb-2`}
                     onClick={(e) => {
                       e.preventDefault();
                       setIsDrawerOpen(!isDrawerOpen);
-                    }}
-                  >
+                    }}>
                     <Triangle
-                      className={`w-5 h-5 bottom-0 ${isDrawerOpen ? 'rotate-180' : ''}`}
+                      className={`w-5 h-5 bottom-0 ${
+                        isDrawerOpen ? "rotate-180" : ""
+                      }`}
                       fill="black"
                     />
                   </motion.div>
                 </TooltipTrigger>
-                <TooltipContent>{isDrawerOpen ? 'Close' : 'Open'}</TooltipContent>
+                <TooltipContent>
+                  {isDrawerOpen ? "Close" : "Open"}
+                </TooltipContent>
               </Tooltip>
             </TooltipProvider>
 
--- src/app/(dashboard)/notes/_components/index.ts
+++ src/app/(dashboard)/notes/_components/index.ts
@@ -1,2 +1,1 @@
 export * from './NewNote';
-export * from './Notes';
