f031bad703393bf740a9b76db3601bffafc9c2c7
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-17 15:13:34 +0700

    chore: delete redundant api call in the client side

--- src/app/api-handlers/api-handler.tsx
+++ src/app/api-handlers/api-handler.tsx
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-export const getYoutubeResponse = async (input: string) => {
-  const params: { query: string; pageToken?: string | null } = {
-    query: input,
-  };
-
-  const response = await axios.get('/api/v1/youtube', { params });
-
-  return { data: response.data };
-};
-
 export const saveOutput = async (input: string, myLearningId: string) => {
   const processInputResponse = await processInput(input, myLearningId);
 
