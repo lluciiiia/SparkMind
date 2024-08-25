81f460fa3c9fcec42d166516449c94e7a36df720
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-08-26 02:18:36 +0700

    chore: cleaner code in saveOutput

--- src/app/api-handlers/api-handler.tsx
+++ src/app/api-handlers/api-handler.tsx
@@ -1,47 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const saveOutput = async (input: string, myLearningId: string) => {
-  const processInputResponse = await processInput(input, myLearningId);
+  const processInputResponse = await handleRequest(() =>
+    processInput(input, myLearningId)
+  );
+  const outputId = processInputResponse.data.outputId;
 
-  if (processInputResponse.status === 200) {
-    const outputId = processInputResponse.data.outputId;
-    const processFinalizingResponse = await processFinalizing(input, myLearningId, outputId);
+  await handleRequest(() => processFinalizing(input, myLearningId, outputId));
+  await handleRequest(() => processActionItems(myLearningId, outputId));
+};
 
-    if (processFinalizingResponse.status === 200) {
-      const processActionItemsResponse = await processActionItems(myLearningId, outputId);
-      if (processActionItemsResponse.status != 200)
-        throw new Error('processActionItems failed : ' + processActionItemsResponse);
-    } else {
-      throw new Error('processFinalizing failed : ' + processFinalizingResponse);
-    }
-  } else {
-    throw new Error('processInput failed : ' + processInputResponse);
+const handleRequest = async (request: () => Promise<any>) => {
+  const response = await request();
+  if (response.status !== 200) {
+    throw new Error(`${request.name} failed: ` + response);
   }
+  return response;
 };
 
 const processInput = async (input: string, myLearningId: string) => {
-  return await axios.post(`/api/v1/outputs/input-processing?id=${myLearningId}`, {
+  return axios.post(`/api/v1/outputs/input-processing?id=${myLearningId}`, {
     input: input,
   });
 };
 
-const processFinalizing = async (input: string, myLearningId: string, outputId: string) => {
-  return await axios.post(
+const processFinalizing = async (
+  input: string,
+  myLearningId: string,
+  outputId: string
+) => {
+  return axios.post(
     `/api/v1/outputs/finalize-processing?id=${myLearningId}&output-id=${outputId}`,
-    {
-      input: input,
-    },
+    { input: input }
   );
 };
 
 const processActionItems = async (myLearningId: string, outputId: string) => {
-  return await axios.post(
-    `/api/v1/outputs/action-items-processing?id=${myLearningId}&output-id=${outputId}`,
+  return axios.post(
+    `/api/v1/outputs/action-items-processing?id=${myLearningId}&output-id=${outputId}`
   );
 };
 
 export const processDefaultTitle = async (myLearningId: string) => {
-  return await axios.patch(`/api/v1/outputs/learning-title-processing?id=${myLearningId}`);
+  return axios.patch(
+    `/api/v1/outputs/learning-title-processing?id=${myLearningId}`
+  );
 };
 
 export const getOutput = async (myLearningId: string) => {
@@ -49,12 +52,12 @@ export const getOutput = async (myLearningId: string) => {
     id: myLearningId,
   };
 
-  const response = await axios.get('/api/v1/outputs', { params });
+  const response = await axios.get("/api/v1/outputs", { params });
   return { data: response.data };
 };
 
 export const createEvents = async (selectedTask: any, myLearningId: string) => {
-  return await axios.post('/api/v1/events', {
+  return await axios.post("/api/v1/events", {
     selectedTask: selectedTask,
     learningId: myLearningId,
   });
