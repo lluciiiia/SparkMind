949117c37abca7a81f6f861803449f62af974ace
Mike Odnis <airwolf635@gmail.com>
2024-08-29 09:26:48 -0400

    chore: revising error and success logic, and ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,7 +93,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), addVariablesForColors],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography'), addVariablesForColors],
 } satisfies Config;
 
 export default config;
